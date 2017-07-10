521bcdca66b87e4528ba08c41c2d95352f09855a
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-07-10 13:36:45 +0200

    End_State

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log(Time.timeSinceLevelLoad + " Starting Host.");
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Host started.");
+    }
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        //base.OnStartClient(client);
+        Debug.Log(Time.timeSinceLevelLoad + " Cient Start Requested." );
+        InvokeRepeating("PrintDots", 0f, 1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        //base.OnClientConnect(conn);
+        Debug.Log(Time.timeSinceLevelLoad + " Cient is connected to IP: " + conn.address);
+        CancelInvoke();
+    }
+
+    void PrintDots()
+    {
+        Debug.Log(".");
+    }
+}
